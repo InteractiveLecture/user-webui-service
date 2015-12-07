2df51f443422d8ff7857b70075cba10f3cb69b0d
Michael Cremer <cremerm@hochschule-trier.de>
2015-12-07 22:50:19 +0100

    Upload: Html ausarbeiten und Controller Mock einfügen

--- app/tutorialTextUpload/tutorial-text-upload-controller.ts
+++ app/tutorialTextUpload/tutorial-text-upload-controller.ts
@@ -4,16 +4,23 @@ module TutorialTextUploadCtrl {
 
   class TutorialTextUploadCtrl {
 
+    tutorialName: string
+    tutorialDescription: string
+
     // $inject annotation.
     // It provides $injector with information about dependencies to be injected into constructor
     // it is better to have it close to the constructor, because the parameters must match in count and type.
     // See http://docs.angularjs.org/guide/di
-    public static $inject :any= [
+    public static $inject: any = [
     ];
 
     // dependencies are injected via AngularJS $injector
     constructor() {
       var vm = this;
+    }
+
+    uploadText(description: string, name: string) {
+      // Do some Upload Magic
     }
   }
 
--- app/tutorialVideoUpload/tutorial-video-upload-controller.ts
+++ app/tutorialVideoUpload/tutorial-video-upload-controller.ts
@@ -4,16 +4,22 @@ module TutorialVideoUploadCtrl {
 
   class TutorialVideoUploadCtrl {
 
+    videoPath: string
+    videoName: string
     // $inject annotation.
     // It provides $injector with information about dependencies to be injected into constructor
     // it is better to have it close to the constructor, because the parameters must match in count and type.
     // See http://docs.angularjs.org/guide/di
-    public static $inject :any= [
+    public static $inject: any = [
     ];
 
     // dependencies are injected via AngularJS $injector
     constructor() {
       var vm = this;
+    }
+
+    uploadVideo(file:any, name:string) {
+      // Do some Upload Magic
     }
   }
 
